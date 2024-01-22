6b17fbab6c6e31477e1eefbd38a391101a919619
xmodpt <xmodpt@gmail.com>
2024-01-22 00:54:15 +0000

    updated

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ plugin_package = "octoprint_chituboard"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "Octoprint-Chituboard"
+plugin_name = "Octoprint-Chituboard-Fixed"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.2.0.Beta"
+plugin_version = "1.0.4.1"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
@@ -27,7 +27,7 @@ plugin_author = "Xmodpt, based on work by Vikram Sarkhel and Philipp Herbrich"
 plugin_author_email = "xmodpt@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/xmodpt/Octoprint-Chituboard"
+plugin_url = "https://github.com/xmodpt/Chituboard-for-Octoprint-1.9.x"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
